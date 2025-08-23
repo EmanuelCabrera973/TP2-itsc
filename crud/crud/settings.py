75677d2e8c078c0da0f76a687b905bd9be57c536
EmanuelCabrera973 <emanuel.cabrera@itscordoba.edu.ar>
2025-08-23 07:57:38 +0200

    diseño de los modelsos

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+# CONFIGURACION PARA EL STATCI
+
+STATIC_URL = '/static/'
--- crud/persona/models.py
+++ crud/persona/models.py
@@ -1,2 +1,12 @@
 from django.db import models
+from oficina.models import Oficina
 
+class Persona(models.Model):
+    nombre = models.CharField(max_Length=100)
+    apellido = models.CharField(max_Lenght=100)
+    edad = models.IntergerField()
+    oficina = models.ForeignKey(Oficina, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.nombre}, {self.apellido}"
+    
